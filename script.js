5a55632cf020c307c4ea4cbbe65332ffd1f43d14
Janos SZABO <jszabo@e2r8p11.42.fr>
2018-06-04 13:11:44 +0200

    started ui

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var info;
+var name;
 
 navigator.geolocation.getCurrentPosition(success, error);
 
@@ -9,7 +10,8 @@ function success(position) {
     var lng = position.coords.longitude;
  $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&APPID=a9e4b8f8e32736da449d4bd4207895e3&units=metric', function(data) {
     info = JSON.stringify(data);
-    document.getElementById("div1").innerHTML = info;
+    name = JSON.stringify(data.name);
+    document.getElementById("city").innerHTML = name;
 });
 }
 
